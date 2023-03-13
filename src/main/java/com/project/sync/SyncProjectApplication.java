a8e3ea9963c5ace176ce898ab18971e7a6f3ffae
Steve Bachta <sbachta90@gmail.com>
2023-03-12 22:20:58 -0400

    Gradle initialize

---
+++ src/main/java/com/project/sync/SyncProjectApplication.java
@@ -0,0 +1,13 @@
+package com.project.sync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyncProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/sync/SyncProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.sync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
